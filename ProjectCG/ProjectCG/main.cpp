dd2dcf9ac15afcb8f1d72ece9c5dc39eb7e1acbe
Pedro Chicória <pedrochicoria@MacBook-Pro-de-Pedro-5.local>
2019-03-12 15:22:46 +0000

    SkyBox done

--- ProjectCG/ProjectCG/main.cpp
+++ ProjectCG/ProjectCG/main.cpp
@@ -8,299 +8,271 @@
 #include <GLUT/glut.h>
 
 //--------------------------------- Definir cores
-#define BLUE     0.0, 0.0, 1.0, 1.0
-#define RED         1.0, 0.0, 0.0, 1.0
-#define YELLOW     1.0, 1.0, 0.0, 1.0
-#define GREEN    0.0, 1.0, 0.0, 1.0
-#define WHITE    1.0, 1.0, 1.0, 1.0
-#define BLACK    0.0, 0.0, 0.0, 1.0
-#define PI         3.14159
+#define BLUE 0.0, 0.0, 1.0, 1.0
+#define RED 1.0, 0.0, 0.0, 1.0
+#define YELLOW 1.0, 1.0, 0.0, 1.0
+#define GREEN 0.0, 1.0, 0.0, 1.0
+#define WHITE 1.0, 1.0, 1.0, 1.0
+#define BLACK 0.0, 0.0, 0.0, 1.0
+#define PI 3.14159
 
-//================================================================================
-//===========================================================Variaveis e constantes
+GLfloat tamanho = 70.0;
+GLfloat tamanhoY = 40.0;
 
 //------------------------------------------------------------ Sistema Coordenadas + objectos
-GLint        wScreen=800, hScreen=600;        //.. janela (pixeis)
-GLfloat        xC=10.0, yC=10.0, zC=10.0;        //.. Mundo  (unidades mundo)
+GLint wScreen = 800, hScreen = 600; //.. janela (pixeis)
+GLuint height = 600, width = 600;
+GLfloat xC = 15.0, yC = 15.0, zC = 70.0; //.. Mundo  (unidades mundo)
 
 //------------------------------------------------------------ Observador
-GLfloat  rVisao=10, aVisao=0.5*PI, incVisao=0.05;
-GLfloat  obsP[] ={rVisao*cos(aVisao), 3.0, rVisao*sin(aVisao)};
-GLfloat  angZoom=90;
-GLfloat  incZoom=3;
+GLfloat rVisao = 20, aVisao = PI + PI / 2, incVisao = 0.05;
+GLfloat obsP[] = {0, -20, 20}; //estava a 320
+GLfloat obsF[] = {0.0, -20, 0.0};
+GLfloat angZoom = 80;
+GLfloat incZoom = 3;
+GLuint movement = -1; //-1 para fora do edificio | 0  o res de chão | 1 para o primeiro movement
+GLfloat movimentoX, movimentoZ, movimentoNormal;
 
+void criaDefineTexturas();
+void drawSkybox();
 
-//=========================================================== FACES DA MESA
-GLboolean   frenteVisivel=1;
-static GLuint     cima[] = {8,11, 10,  9};
-static GLuint     esquerda[]= {0, 1, 2, 3};
-static GLuint     direita[ ]= {4, 7, 6, 5};;
+// Função responsável pela especificação dos parâmetros de iluminação
 
-
-
-GLfloat tam=2.0;
-static GLfloat vertices[]={
-    //�������������������������������������� x=tam (Esquerda)
-    -tam,  -tam,  tam,    // 0
-    -tam,   tam,  tam,    // 1
-    -tam,   tam, -tam,    // 2
-    -tam,  -tam, -tam,    // 3
-    //�������������������� Direita
-    tam,  -tam,  tam,    // 4
-    tam,   tam,  tam,    // 5
-    tam,   tam, -tam,    // 6
-    tam,  -tam, -tam,    // 7
-    //��������������������� (Cima
-    -tam,  tam,  tam,    // 8
-    -tam,  tam, -tam,    // 9
-    tam,  tam, -tam,    // 10
-    tam,  tam,  tam,    // 11
-};
-
-static GLfloat normais[] = {
-    //�������������������������������������� x=tam (Esquerda)
-    -1.0,  0.0,  0.0,
-    -1.0,  0.0,  0.0,
-    -1.0,  0.0,  0.0,
-    -1.0,  0.0,  0.0,
-    //�������������������������������������� x=tam (Direita)
-    1.0,  0.0,  0.0,
-    1.0,  0.0,  0.0,
-    1.0,  0.0,  0.0,
-    1.0,  0.0,  0.0,
-    //�������������������������������������� y=tam (Cima)
-    0.0,  1.0,  0.0,
-    0.0,  1.0,  0.0,
-    0.0,  1.0,  0.0,
-    0.0,  1.0,  0.0,
-};
-//------------------------------------------------------------ Cores
-static GLfloat cores[]={
-    //�������������������������������������� x=tam (Esquerda) - Red
-    1.0,  0.0, 0.0,    // 0
-    1.0,  0.0, 0.0,    // 1
-    1.0,  1.0, 0.0,    // 2
-    1.0,  1.0, 0.0,    // 3
-    //�������������������������������������� x=2*tam (Direita) - Green
-    0.0,  1.0, 1.0,    // 4
-    0.0,  1.0, 1.0,    // 5
-    0.0,  1.0, 0.0,    // 6
-    0.0,  1.0, 0.0,    // 7
-    //�������������������������������������� y=tam (Cima) - Blue
-    0.0,  0.0, 1.0,    // 8
-    0.0,  0.0, 1.0,    // 9
-    1.0,  0.0, 1.0,    // 10
-    1.0,  0.0, 1.0,    // 11
-};
-
-
-
-
-
-
-
-
-
-//================================================================================
-//=========================================================================== INIT
-void inicializa(void)
+void init(void)
 {
-    glClearColor(BLACK);        //������������������������������Apagar
-    glEnable(GL_DEPTH_TEST);    //������������������������������Profundidade
-    glShadeModel(GL_SMOOTH);    //������������������������������Interpolacao de cores
     
-    glEnable(GL_CULL_FACE);        //������������������������������Faces visiveis
-    glCullFace(GL_BACK);        //������������������������������Mostrar so as da frente
+    glClearColor(WHITE);     //Apagar
+    glEnable(GL_DEPTH_TEST); //Profundidade
+    glEnable(GL_BLEND);
+    glShadeModel(GL_SMOOTH);
     
-    glVertexPointer(3, GL_FLOAT, 0, vertices); //���������������Vertex arrays
-    glEnableClientState(GL_VERTEX_ARRAY);
-    glNormalPointer(GL_FLOAT, 0, normais);
-    glEnableClientState(GL_NORMAL_ARRAY);
-    glColorPointer(3, GL_FLOAT, 0, cores);
-    glEnableClientState(GL_COLOR_ARRAY);
+    glEnable(GL_COLOR_MATERIAL); // Habilita a definição da cor do material a partir da cor corrente
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
 }
 
-
-
-void drawEixos()
+void drawSkybox()
 {
-    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Eixo X
-    glColor4f(RED);
-    glBegin(GL_LINES);
-    glVertex3i( 0, 0, 0);
-    glVertex3i(10, 0, 0);
-    glEnd();
-    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Eixo Y
-    glColor4f(GREEN);
-    glBegin(GL_LINES);
-    glVertex3i(0,  0, 0);
-    glVertex3i(0, 10, 0);
-    glEnd();
-    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Eixo Z
+    
+    //atrás
+    
+    glPushMatrix();
     glColor4f(BLUE);
-    glBegin(GL_LINES);
-    glVertex3i( 0, 0, 0);
-    glVertex3i( 0, 0,10);
+    glTranslated(0, 0, -45);
+    glBegin(GL_QUADS);
+    glVertex3i(-800, 600, -250);
+    glVertex3i(800, 600, -250);
+    glVertex3i(800, -600, -250);
+    glVertex3i(-800, -600, -250);
     glEnd();
-    
-}
-
-
-
-
-void drawScene(){
-    
-    //=================================================== Qual o lado ?
-    if (frenteVisivel)
-        glCullFace(GL_BACK);  //glFrontFace(GL_CW);
-    else
-        glCullFace(GL_FRONT);  //glFrontFace(GL_CCW);
-    
-    
-    //==================================== MESA
-    //glColorPointer(3, GL_FLOAT, 0, cor);     podia ser modificada a cor !
-    glPushMatrix();
-    //?? esquerda
-    //?? direita
-    glDrawElements(GL_POLYGON, 4, GL_UNSIGNED_INT, cima);
-    glDrawElements(GL_POLYGON, 4, GL_UNSIGNED_INT, direita);
-    glDrawElements(GL_POLYGON, 4, GL_UNSIGNED_INT, esquerda);
     glPopMatrix();
     
+    //esquerda
     
-    //==================================== PAralelipipedo Amarelo
-    glColor4f(YELLOW);
     glPushMatrix();
-    //?? escala, rotacao, translacao ??
-    glTranslated(0, 3, 0);
-    glutWireTeapot(1);
+    glColor4f(BLUE);
+    glTranslated(0, 0, -45);
+    glBegin(GL_QUADS);
+    glVertex3i(-250, -600, 800);
+    glVertex3i(-250, 600, 800);
+    glVertex3i(-250, 600, -800);
+    glVertex3i(-250, -600, -800);
+    glEnd();
     glPopMatrix();
     
+    //direita
+    
+    glPushMatrix();
+    glColor4f(BLUE);
+    glTranslated(0, 0, -45);
+    glBegin(GL_QUADS);
+    glVertex3i(250, -600, 800);
+    glVertex3i(250, 600, 800);
+    glVertex3i(250, 600, -800);
+    glVertex3i(250, -600, -800);
+    glEnd();
+    glPopMatrix();
+    
+    //chão
+    
+    glPushMatrix();
+    glColor4f(GREEN);
+    glTranslated(0, 0, -45);
+    glBegin(GL_QUADS);
+    glVertex3i(-800, -tamanhoY - 10, -800);
+    glVertex3i(-800, -tamanhoY - 10, 800);
+    glVertex3i(800, -tamanhoY - 10, 800);
+    glVertex3i(800, -tamanhoY - 10, -800);
+    glEnd();
+    glPopMatrix();
+    
+    //Frente
+    
+    glPushMatrix();
+    glColor4f(BLUE);
+    glTranslated(0, 0, -45);
+    glBegin(GL_QUADS);
+    glVertex3i(-800, 600, 650);
+    glVertex3i(800, 600, 650);
+    glVertex3i(800, -600, 650);
+    glVertex3i(-800, -600, 650);
+    glEnd();
+    glPopMatrix();
+    
+    // -------------------------Ceu-------------------
+    
+    glPushMatrix();
+    glColor4f(BLUE);
+    glTranslated(0, 0, -45);
+    glBegin(GL_QUADS);
+    glVertex3i(-800, 250, 800);
+    glVertex3i(-800, 250, -800);
+    glVertex3i(800, 250, -800);
+    glVertex3i(800, 250, 800);
+    glEnd();
+    glPopMatrix();
 }
 
-void display(void){
+void drawScene()
+{
+    drawSkybox();
+}
+
+void display(void)
+{
     
-    //================================================================= APaga ecran/profundidade
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+    //================================================================= Apaga ecran/profundidade
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
     
+    //================================================================= No modificar
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glViewport(0, 0, wScreen, hScreen);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(angZoom, (float)wScreen / hScreen, 0.1, 10 * zC);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
     
-    //================================================================= NAO MOFIFICAR
-    glViewport (0, 0, wScreen, hScreen);                                // ESQUECER PoR AGORA
-    glMatrixMode(GL_PROJECTION);                                        // ESQUECER PoR AGORA
-    glLoadIdentity();                                                    // ESQUECER PoR AGORA
-    gluPerspective(angZoom, (float)wScreen/hScreen, 0.1, 3*zC);            // ESQUECER PoR AGORA
-    glMatrixMode(GL_MODELVIEW);                                            // ESQUECER PoR AGORA
-    glLoadIdentity();                                                    // ESQUECER PoR AGORA
-    //================================================================= NAO MOFIFICAR
+    glRotatef(aVisao, 0, 1, 0);
+    gluLookAt(obsP[0], obsP[1], obsP[2], obsF[0], obsF[1], obsF[2], 0, 1, 0);
     
+    glEnable(GL_NORMALIZE);
+    //codigo baseado no trabalho de reflexoes de CG
     
-    //-------------------------------------------------------------- observador
-    gluLookAt(obsP[0], obsP[1], obsP[2], 0,0,0, 0, 1, 0);
-    
-    //����������������������������������������������������������Objectos/modelos
-    drawEixos();
     drawScene();
     
-    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Actualizacao
+    //Actualizacao
     glutSwapBuffers();
 }
 
-
 //======================================================= EVENTOS
-void keyboard(unsigned char key, int x, int y){
+void keyboard(unsigned char key, int x, int y)
+{
     
-    
-    switch (key) {
-        case 'f':
-        case 'F':
-            frenteVisivel=!frenteVisivel;
-            glutPostRedisplay();
-            break;
-            
-        case 'A':
-        case 'a':
-            //??
-            glutPostRedisplay();
-            break;
+    switch (key)
+    {
             
         case 'S':
         case 's':
-            //??
+            movimentoX = obsF[0] - obsP[0];
+            movimentoZ = obsF[2] - obsP[2];
+            movimentoNormal = sqrt(pow(movimentoX, 2) + pow(movimentoZ, 2));
+            movimentoX = movimentoX / movimentoNormal;
+            movimentoZ = movimentoZ / movimentoNormal;
+            obsP[0] = obsP[0] - movimentoX;
+            obsP[2] = obsP[2] - movimentoZ;
+            obsF[0] = obsF[0] - movimentoX;
+            obsF[2] = obsF[2] - movimentoZ;
             glutPostRedisplay();
             break;
             
-        case 'e':
-        case 'E':
-            //??
+        case 'w':
+        case 'W':
+            movimentoX = obsF[0] - obsP[0];
+            movimentoZ = obsF[2] - obsP[2];
+            movimentoNormal = sqrt(pow(movimentoX, 2) + pow(movimentoZ, 2));
+            movimentoX = movimentoX / movimentoNormal;
+            movimentoZ = movimentoZ / movimentoNormal;
+            obsP[0] = obsP[0] + movimentoX * 2;
+            obsP[2] = obsP[2] + movimentoZ * 2;
+            obsF[0] = obsF[0] + movimentoX * 2;
+            obsF[2] = obsF[2] + movimentoZ * 2;
+      
             glutPostRedisplay();
             break;
             
-        case 'd':
-        case 'D':
-            //??
+        case 'o':
+        case 'O':
+            
+            glutPostRedisplay();
+            break;
+            //--------------------------- Rotate lateral na lata
+        case 'p':
+        case 'P':
+            
             glutPostRedisplay();
             break;
             
+        case 'u':
             
+            break;
             
+        case 'l':
+        case 'L':
             
+            glutPostRedisplay();
+            break;
             
+        case 'z':
+        case 'Z':
+            
+            glutPostRedisplay();
+            break;
             //--------------------------- Escape
         case 27:
             exit(0);
             break;
     }
-    
+}
+void idle(void)
+{
+    glutPostRedisplay();
 }
 
+void teclasNotAscii(int key, int x, int y)
+{
+    
+    if (key == GLUT_KEY_LEFT)
+        aVisao = (aVisao - 0.05);
+    if (aVisao > 2 * PI)
+        aVisao = 0;
+    if (key == GLUT_KEY_RIGHT)
+        aVisao = (aVisao + 0.05);
 
-
-void teclasNotAscii(int key, int x, int y){
+    if (aVisao < -2 * PI)
+        aVisao = 0;
     
-    if(key == GLUT_KEY_UP)
-        obsP[1] = (obsP[1]+ 0.1) ;
-    if(key == GLUT_KEY_DOWN)
-        obsP[1] = (obsP[1]- 0.1) ;
-    
-    if (obsP[1]>yC)
-        obsP[1]=yC;
-    if (obsP[1]<-yC)
-        obsP[1]=-yC;
-    
-    if(key == GLUT_KEY_LEFT)
-        aVisao = (aVisao + 0.1) ;
-    if(key == GLUT_KEY_RIGHT)
-        aVisao = (aVisao - 0.1) ;
-    
-    obsP[0]=rVisao*cos(aVisao);
-    obsP[2]=rVisao*sin(aVisao);
-    
+    obsF[0] = obsP[0] + rVisao * cos(aVisao);
+    obsF[2] = obsP[2] + rVisao * sin(aVisao);
     
     glutPostRedisplay();
-    
 }
 
-
-//======================================================= MAIN
-//======================================================= MAIN
-int main(int argc, char** argv){
+int main(int argc, char **argv)
+{
     
     glutInit(&argc, argv);
-    glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH );
-    glutInitWindowSize (wScreen, hScreen);
-    glutInitWindowPosition (300, 100);
-    glutCreateWindow ("Projeto Computação Gráfica 2019/2019 | João Figueiredo | Pedro Chicória");
-    
-    inicializa();
-    
-    glutSpecialFunc(teclasNotAscii);
-    glutDisplayFunc(display);
-    glutKeyboardFunc(keyboard);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(wScreen, hScreen);
+    glutInitWindowPosition(300, 100);
+    glutCreateWindow(" Projeto Computação Gráfica ");
+    init();
+    glutSpecialFunc(teclasNotAscii); //para as setas
+    glutDisplayFunc(display);        //display callback
+    glutKeyboardFunc(keyboard);      //para as letras
+    glutIdleFunc(display);
     
     glutMainLoop();
     
     return 0;
 }
-
-
-
